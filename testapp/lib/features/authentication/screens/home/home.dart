16ff1a75b78761d63ed93d95c080dd12ccf87dd4
ZeRp-LLa <mushyk.illia.s@gmail.com>
2025-04-02 01:24:41 +0300

    Custom Shape

--- testapp/lib/features/authentication/screens/home/home.dart
+++ testapp/lib/features/authentication/screens/home/home.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:testapp/common/custom_shapes/containers/circular_container.dart';
+import 'package:testapp/common/custom_shapes/containers/primary_header_container.dart';
+import 'package:testapp/common/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:testapp/common/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:testapp/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [TPrimaryHeaderContainer(child: Container())]),
+      ),
+    );
   }
 }
